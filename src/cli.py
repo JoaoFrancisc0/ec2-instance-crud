7ad22f097923d252ba1924eb588894b54e4376e6
Ytalul <ytalogbr4@gmail.com>
2025-08-29 23:14:37 -0300

    Feat: Issue 2 CLI adicionado

--- src/cli.py
+++ src/cli.py
@@ -1,4 +1,31 @@
-from src import click, tabulate, svc
+from src import ec2_service as svc
 
 def main():
-    print("Hello World!")
+    print("1- Criar nova instância de máquina virtual\n" \
+          "2- Listar todas as instâncias de máquinas virtuais\n" \
+          "3- Iniciar uma instância de máquina virtual\n" \
+          "4- Parar uma instância de máquina virtual\n" \
+          "5- Terminar uma instância de máquina virtual\n" \
+          "0- Sair\n")
+    escolha = input("Escolha uma opção: ")
+    if escolha == "1":
+        nome = input("Nome da instância: ")
+        svc.criar_instancia(nome)
+    elif escolha == "2":
+        svc.listar_instancias()
+    elif escolha == "3":
+        id = input("ID da instância: ")
+        svc.iniciar_instancia(id)
+    elif escolha == "4":
+        id = input("ID da instância: ")
+        svc.parar_instancia(id)
+    elif escolha == "5":
+        id = input("ID da instância: ")
+        svc.terminar_instancia(id)
+    elif escolha == "0":
+        print("Saindo...")
+    else:
+        print("Opção inválida.")
+
+if __name__ == "__main__":
+    main()
