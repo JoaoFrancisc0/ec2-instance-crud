382995ba7947e30e0a18cf84c95d749500f77407
Ytalul <ytalogbr4@gmail.com>
2025-08-29 23:21:16 -0300

    Feat: Função Creat adicionada

--- src/ec2_service.py
+++ src/ec2_service.py
@@ -1,5 +1,22 @@
-from src import ClientError
+import boto3
+from botocore.exceptions import ClientError
 
-# Initialize a boto3 session and EC2 resource
+# Inicializa o cliente EC2
+ec2 = boto3.client('ec2')
 
-# CRUD operations for EC2 instances
+def criar_instancia(nome):
+    try:
+        instancia = ec2.run_instances(
+            ImageId='ami-0c94855ba95c71c99', # Substitua pelo seu AMI
+            MinCount=1,
+            MaxCount=1,
+            InstanceType='t2.micro',
+            TagSpecifications=[{
+                'ResourceType': 'instance',
+                'Tags': [{'Key': 'Name', 'Value': nome}]
+            }]
+        )
+        print(f"Instância criada: {instancia['Instances'][0]['InstanceId']}")
+    except ClientError as e:
+        print(f"Erro ao criar instância: {e}")
+
